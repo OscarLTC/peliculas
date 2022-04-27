d1c42d2176dc45dac553af4a77aba557501bbf59
Oscar <oscartaco2003@gmail.com>
2022-04-26 23:06:00 -0500

    enviroment

--- src/utils/httpClient.js
+++ src/utils/httpClient.js
@@ -1,11 +1,11 @@
-const API = "https://api.themoviedb.org/3";
+const API = process.env.REACT_APP_API;
+const API_TOKEN = process.env.REACT_APP_API_TOKEN;
 
 export function get(path) {
-    return fetch(API + path, {
-        headers: {
-            Authorization:
-                "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MzNjMmM0YTU0ZjNhZDllOTU0NzQ2MDdiYWEwYzk2ZCIsInN1YiI6IjYyMjEyMWFjNDJiZjAxMDAxZDA1ZTdmZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.c65guegIP3XF0WFHcMsFVSbnnNbIHxCaNPSj6iZqdUg",
-            "Content-Type": "application/json;charset=utf-8",
-        },
-    }).then((result) => result.json())
+  return fetch(API + path, {
+    headers: {
+      Authorization: "Bearer " + API_TOKEN,
+      "Content-Type": "application/json;charset=utf-8",
+    },
+  }).then((result) => result.json());
 }
